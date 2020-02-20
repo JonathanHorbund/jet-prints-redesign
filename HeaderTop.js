170bc122a98f95e380ac67e7d3be50bd7778e3cd
JonathanHorbund <jhorbund@gatech.edu>
2020-02-20 12:42:49 -0500

    1 file modified

--- HeaderTop.js
+++ HeaderTop.js
@@ -1,1 +1,17 @@
+import React from 'react';
+import SocialMedia from './SocialMedia'
+import Cart from './Cart'
 
+let cartCount = {itemNums: 0}
+
+const HeaderTop = () => {
+  return (
+    <section>
+      <SocialMedia />
+      <Cart {...cartCount}/>
+    </section>
+  )
+}
+
+
+export default HeaderTop;
