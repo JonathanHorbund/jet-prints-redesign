3dd8acd0327a7566c9b53559722659ccbc3d3cd5
JonathanHorbund <jhorbund@gatech.edu>
2020-02-20 11:57:47 -0500

    1 file modified

--- SocialMedia.js
+++ SocialMedia.js
@@ -2,8 +2,9 @@ import React from 'react';
 import Icon from './Icon';
 
 const socialMediaArr = [
-  {source: "https://image.flaticon.com/icons/svg/1051/1051382.svg", cat: "social-media-icon", name: "twitter-icon", alt: "Twitter Logo" }
-
+  {source: "https://image.flaticon.com/icons/svg/1384/1384031.svg", cat: "social-media-icon", name: "instagram-icon", alt: "Instagram Logo" },
+  {source: "https://image.flaticon.com/icons/svg/1051/1051382.svg", cat: "social-media-icon", name: "twitter-icon", alt: "Twitter Logo" },
+  {source: "https://image.flaticon.com/icons/svg/1051/1051309.svg", cat: "social-media-icon", name: "facebook-icon", alt: "Facebook Logo" },
 ]
 
 const SocialMedia = () => {
