5b7c33fd3f2ac129ba54ecb13c820567cadf1442
JonathanHorbund <jhorbund@gatech.edu>
2020-02-20 12:51:30 -0500

    1 file modified

--- HeaderTop.js
+++ HeaderTop.js
@@ -2,7 +2,7 @@ import React from 'react';
 import SocialMedia from './SocialMedia'
 import Cart from './Cart'
 
-let cartCount = {numItems: "0", cat: "cart-container", source: "https://image.flaticon.com/icons/svg/1170/1170678.svg", name: "cart-icon", desc: "Shopping Cart", textName: "cart-num-items"}
+let cartCount = {numItems: "0"}
 
 const HeaderTop = () => {
   return (
