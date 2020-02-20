1910bd6bc80f4bccfa675ed7cc8a6cad54023f48
JonathanHorbund <jhorbund@gatech.edu>
2020-02-20 18:17:40 -0500

    1 file modified

--- SiteSearchBar.js
+++ SiteSearchBar.js
@@ -1,1 +1,14 @@
+import React from 'react'
 
+const SiteSearchBar = () => {
+  return (
+    <section id="site-search-bar">
+      <button><img id="search-icon" src="https://image.flaticon.com/icons/svg/483/483356.svg"/></button>
+      <input id="search-bar" type="text" placeholder="Search our entire store" />
+    </section>
+  )
+}
+
+
+
+export default SiteSearchBar
