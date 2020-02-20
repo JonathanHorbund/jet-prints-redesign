29321ce08c9dd044fe269df04e41f7bdaab6cb29
JonathanHorbund <jhorbund@gatech.edu>
2020-02-20 00:51:21 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# jet-prints-redesign-1-0-0
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/jet-prints-redesign-1-0-0)
---
+++ index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render } from 'react-dom';
+import './style.css';
+
+
+const App = () => (
+  <main>
+    <h1>Warmup activity!</h1>
+  </main>
+)
+
+render(<App />, document.querySelector('#root'));
