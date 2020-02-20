d54044e88727c6b62957d4f2f4b4787d04f33601
JonathanHorbund <jhorbund@gatech.edu>
2020-02-20 12:20:43 -0500

    1 file modified

--- Cart.js
+++ Cart.js
@@ -1,1 +1,12 @@
+import React from 'react';
 
+const Cart = (props) => {
+  return (
+    <section class="cart-container">
+      <img src={props.source} id={props.imgName} alt={props.alt} />
+      <p id={props.itemNum}>{props.numItems}</p>
+    </section>
+  )
+}
+
+export default Cart;
