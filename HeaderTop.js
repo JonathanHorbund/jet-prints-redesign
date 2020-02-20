e5082e1fd76896671487420c89df3e70fc7011af
JonathanHorbund <jhorbund@gatech.edu>
2020-02-20 13:25:49 -0500

    2 files modified

--- HeaderTop.js
+++ HeaderTop.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import SocialMedia from './SocialMedia'
 import Cart from './Cart'
+import SignIn from './SignIn'
 
 let cartCount = {numItems: "0"}
 
@@ -8,6 +9,7 @@ const HeaderTop = () => {
   return (
     <section class="header-top">
       <SocialMedia />
+      <SignIn />
       <Cart {...cartCount}/>
     </section>
   )
--- SignIn.js
+++ SignIn.js
@@ -1,1 +1,13 @@
+import React from 'react';
 
+const SignIn = () => {
+  return (
+  <section class="sign-in-container">
+      <img id="sign-in-icon" src="https://image.flaticon.com/icons/svg/1077/1077114.svg" alt="Sign In Icon" />
+      <p>Sign In</p>
+    </section>
+  )
+  
+}
+
+export default SignIn;
