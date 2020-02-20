1638db92f02d950ed6e380527b7403213b93a95c
JonathanHorbund <jhorbund@gatech.edu>
2020-02-20 12:42:30 -0500

    1 file modified

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import { render } from 'react-dom';
-import SocialMedia from './SocialMedia'
-import './style.css';
+import React from 'react';
+import { render } from 'react-dom';
+import HeaderTop from './HeaderTop'
+import './style.css';
 
 
-const App = () => (
-  <main>
-    <SocialMedia />
-  </main>
+const App = () => (
+  <main>
+    <HeaderTop />
+  </main>
 )
 
-render(<App />, document.querySelector('#root'));
+render(<App />, document.querySelector('#root'));
+
